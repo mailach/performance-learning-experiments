0a11451ee53c573922e85822cabc1af427422954
Alina Mailach <alina.mailach@informatik.uni-leipzig.de>
2022-09-12 16:32:10 +0200

    Add transformation from xml to one hot encoded

---
+++ helpers/transformation/data.py
@@ -0,0 +1,48 @@
+import xml.etree.ElementTree as ET
+from typing import Sequence
+import pandas as pd
+
+
+
+
+def features_from_dimacs(dimacs_file: str) -> dict[int, str]:
+
+    with open(dimacs_file, "r") as f:
+        features = {int(line.split()[1]): line.split()[2].strip() for line in f.readlines() if line[0]=="c"}
+
+    return features
+
+
+def _check_feature_existence(config: Sequence[str], features: Sequence[str]) -> None:
+    not_matching = [feature for feature in config if feature not in features]
+    if len(not_matching):
+        raise Exception(f"Can not convert file because '{not_matching}' is/are not listed in the feature model")
+
+
+def _one_hot_encode(config: Sequence[str], features: Sequence[str], value: str):
+    _check_feature_existence(config, features)
+    oh = {feature: 1 if feature in config else 0 for feature in features}
+    oh["measured_value"] = value
+
+    return oh
+
+def xml_measurements_to_onehot(input_file: str, dimacs_file: str, output_file: str, shema: str) -> None:
+    features = features_from_dimacs(dimacs_file)
+    one_hot = []
+
+    df = ET.parse(input_file).getroot()
+    for row in df:
+        config = [f.strip() for f in row.find(f'.//data[@columname="Configuration"]').text.split(",") if f != ""]
+        one_hot.append(_one_hot_encode(config, features.values(), row.find(f'.//data[@columname="Measured Value"]').text))
+
+
+    pd.DataFrame(one_hot).to_csv(output_file, sep="\t")
+
+
+            
+
+
+
+
+
+
