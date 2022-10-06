0efb84a068cc48733f2c3a46cd52f2264379312d
Alina Mailach <alina.mailach@informatik.uni-leipzig.de>
2022-10-06 13:56:30 +0200

    Evaluation decoupled from learning

--- evaluation.py
+++ evaluation.py
@@ -15,7 +15,6 @@ from rich.logging import RichHandler
 from utils.caching import CacheHandler
 from utils.runs import get_all_runs, update_metrics, update_params
 
-from learning.models import cart, LearnerFactory, Learner
 from learning.metrics import prediction_fault_rate
 
 
@@ -26,45 +25,30 @@ logging.basicConfig(
 )
 
 
-def _load_learner(run_id: str) -> Learner:
-    run = mlflow.get_run(run_id)
+def _evaluate(run_id: str) -> None:
     cache = CacheHandler(run_id)
-    if not cache.existing_cache:
-        logging.info(f"Load artifacts for run {run_id} to cache")
-        download_artifacts(run_id=run_id, dst_path=cache.cache_dir)
-
-    learner = LearnerFactory(run.data.params["method"])
-    learner.load(cache.cache_dir)
-    return learner
-
-
-def _evaluate(run_id: str, test: pd.DataFrame) -> None:
-    learner = _load_learner(run_id)
-    fr = prediction_fault_rate(
-        test["measured_value"], learner.predict(
-            test.drop("measured_value", axis=1))
-    )
+    pred = pd.DataFrame(cache.retrieve("predicted.json"))
+    fr = prediction_fault_rate(pred["measured_value"], pred["predicted"])
     update_metrics(run_id, fr)
     return fr
 
 
-@click.command(
+@ click.command(
     help="Evaluate one or multiple models from prior learning runs.",
     context_settings=dict(
         ignore_unknown_options=True,
         allow_extra_args=True,
     ),
-)
-@click.option("--workflow_id")
-@click.option("--sampling_run_id")
-def evaluate(workflow_id: str, sampling_run_id: str):
-    sampling_cache = CacheHandler(sampling_run_id)
-    test_data = pd.DataFrame(
-        sampling_cache.retrieve("remaining_configurations.json"))
-    learner_runs = get_all_runs("learning", {"workflow_id": workflow_id})
-    logging.info(f"Evaluate models from runs {learner_runs}")
 
-    metrics = [{"id": run_id, "metrics": _evaluate(run_id, test_data)}
+
+)
+@ click.option("--workflow_id")
+def evaluate(workflow_id: str):
+
+    learner_runs = get_all_runs("learning", {"workflow_id": workflow_id})
+    logging.info(f"Evaluate prediction from runs {learner_runs}")
+
+    metrics = [{"id": run_id, "metrics": _evaluate(run_id)}
                for run_id in learner_runs]
     best = sorted(
         metrics, key=lambda d: d['metrics']['mean_fault_rate'])[0]
